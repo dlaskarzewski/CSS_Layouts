e094d0b3cf4699ff98d4d67498447ef3ec7af69d
dlaskarzewski <dlaskarzewski@ccsuvt.org>
2016-05-04 09:27:56 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
-$("menu").hide();
-alert("a");
-$("burger").click(function(){
-  $(this).next().show();
+$("#fullnav").hide();
+$("#imgb").click(function(){
+    this.style.transform= "rotate(" +getNextAngle()+"deg)";
+    $("#fullnav").toggle(1000);
 });
+
+
+var nextAngle = 0;
+function getNextAngle() {
+    nextAngle += 90;    
+    return nextAngle;
+}
